55cf04e91be7bbdb73cb0a66347f2775fe6cda1a
Artem Pylhun <145591080+Artem-Pylhun@users.noreply.github.com>
2024-04-02 23:54:27 +0300

    feat: layout added and used, chart page created

---
+++ layouts/default.vue
@@ -0,0 +1,24 @@
+<script setup lang="ts">
+
+const links = [{
+  label: 'ListOfStudents',
+  to: '/ListOfStudents'
+}, {
+  label: 'Chart',
+  to: '/Chart'
+}]
+</script>
+
+<template>
+  <UHorizontalNavigation :links="links">
+    <template #default="{ link }">
+      <span class="group-hover:text-primary relative">{{ link.label }}</span>
+    </template>
+  </UHorizontalNavigation>
+  <router-view/>
+</template>
+
+
+<style scoped>
+
+</style>
---
+++ pages/Chart.vue
@@ -0,0 +1,12 @@
+<script lang="ts">
+useHead({
+  title: 'Сhart'
+})
+</script>
+
+<template>
+
+</template>
+<style scoped>
+
+</style>
